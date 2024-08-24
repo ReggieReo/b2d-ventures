45b0ae7433170c8d2007bb4cf3804a6634c9a91e
Phumrapee Chaowanapricha <phumrapeechaowanapricha@Phumrapees-MacBook-Pro-2.local>
2024-08-24 16:28:29 +0700

    Modify Portfolio page

--- src/app/investors/page.tsx
+++ src/app/investors/page.tsx
@@ -1,27 +1,66 @@
-export default function portfolioPage() {
+export default function PortfolioPage() {
   return (
-    <div className="w-[1422px] h-[120px] relative">
-        <div className="w-[1800px] h-[120px] p-8 left-0 top-0 absolute bg-white shadow border-b border-[#d9d9d9] justify-start items-center gap-6 inline-flex">
-            <div className="grow shrink basis-0 h-8 justify-end items-center gap-2 flex">
-                <div className="p-2 bg-neutral-100 rounded-lg justify-center items-center gap-2 flex">
-                    <div className="text-[#1e1e1e] text-base font-normal font-['Inter'] leading-none">Investors</div>
-                </div>
-                <div className="p-2 rounded-lg justify-center items-center gap-2 flex">
-                    <div className="text-[#1e1e1e] text-base font-normal font-['Inter'] leading-none">Businesses</div>
-                </div>
+    <div className="w-[1440px] h-[1024px] relative bg-white backdrop-blur-[50px]">
+    <div className="w-[1422px] h-[120px] left-[3px] top-[40px] absolute">
+        <div className="w-[1795px] h-[120px] p-8 left-0 top-0 absolute bg-white shadow border-b border-[#d9d9d9] justify-start items-center gap-6 inline-flex">
+        <div className="grow shrink basis-0 h-8 justify-end items-center gap-2 flex">
+            <div className="p-2 bg-neutral-100 rounded-lg justify-center items-center gap-2 flex">
+            <div className="text-[#1e1e1e] text-base font-normal font-['Inter'] leading-none">Investors</div>
             </div>
-            <div className="px-3 py-2 rounded-lg justify-start items-center gap-2 flex">
-                <div className="w-10 h-10 relative rounded-full">
-                    <img className="w-10 h-10 left-0 top-0 absolute" src="./assets/user.jpg" />
-                </div>
-                <div className="w-4 h-4 relative" />
+            <div className="p-2 rounded-lg justify-center items-center gap-2 flex">
+            <div className="text-[#1e1e1e] text-base font-normal font-['Inter'] leading-none">Businesses</div>
             </div>
         </div>
-        <div className="w-[743.12px] h-10 px-4 py-3 left-[335.75px] top-[40px] absolute bg-white rounded-full border border-[#d9d9d9] justify-start items-center gap-2 inline-flex">
-            <div className="grow shrink basis-0 text-[#b3b3b3] text-base font-normal font-['Inter'] leading-none">Search</div>
+        <div className="px-3 py-2 rounded-lg justify-start items-center gap-2 flex">
+            <div className="w-10 h-10 relative rounded-full">
+            <img className="w-10 h-10 left-0 top-0 absolute" src="./assets/user.jpg" />
+            </div>
             <div className="w-4 h-4 relative" />
         </div>
+        </div>
+        <div className="w-[543.12px] h-10 px-4 py-3 left-[335.75px] top-[40px] absolute bg-white rounded-full border border-[#d9d9d9] justify-start items-center gap-2 inline-flex">
+        <div className="grow shrink basis-0 text-[#b3b3b3] text-base font-normal font-['Inter'] leading-none">Search</div>
+        <div className="w-4 h-4 relative" />
+        </div>
         <img className="w-[166.89px] h-[120px] left-[84.93px] top-0 absolute" src="./assets/b2dlogo.png" />
+    </div>
+    <div className="w-[803px] h-[180px] left-[77px] top-[200px] absolute">
+        <div className="w-[803px] h-[180px] left-0 top-0 absolute bg-[#f1f1f1] rounded-[15px]" />
+        <img className="w-[100px] h-[100px] left-[39px] top-[26px] absolute" src="./assets/rento.png" />
+        <div className="w-[219px] h-[90px] left-[166px] top-[22px] absolute text-black text-5xl font-bold font-['Inter'] leading-[57.60px]">Rento</div>
+        <div className="w-[282px] h-[22px] left-[166px] top-[104px] absolute text-[#494c4f] text-base font-normal font-['Inter'] leading-snug">Peer-to-Peer Rental Platform</div>
+        <div className="w-[100px] h-[25px] left-[39px] top-[140px] absolute bg-white rounded-[5px]" />
+        <div className="w-20 left-[47px] top-[143px] absolute text-center text-[#494c4f] text-sm font-normal font-['Inter'] leading-tight">Technology</div>
+        <div className="w-[100px] h-[25px] left-[149px] top-[140px] absolute bg-white rounded-[5px]" />
+        <div className="w-20 left-[157px] top-[143px] absolute text-center text-[#494c4f] text-sm font-normal font-['Inter'] leading-tight">Real Estate</div>
+    </div>
+    <img className="w-[800px] h-[500px] left-[80px] top-[400px] absolute" src="./assets/Media.png" />
+    <div className="w-[450px] h-[710px] left-[902px] top-[200px] absolute">
+        <div className="w-[450px] h-[710px] left-0 top-0 absolute bg-[#f1f1f1] rounded-[15px]" />
+        <div className="w-[115px] h-[127px] left-[43px] top-[206px] absolute">
+        <div className="w-[114px] h-16 left-[1px] top-0 absolute text-black text-xl font-normal font-['Inter'] leading-normal">Investors</div>
+        <div className="w-[115px] h-16 left-0 top-[63px] absolute text-black text-5xl font-bold font-['Inter'] leading-[57.60px]">41</div>
+        </div>
+        <div className="w-[206px] h-[127px] left-[204px] top-[206px] absolute">
+        <div className="w-[205px] h-16 left-[1px] top-0 absolute text-black text-xl font-normal font-['Inter'] leading-normal">minimum investment </div>
+        <div className="w-[199px] h-16 left-0 top-[63px] absolute text-black text-5xl font-bold font-['Inter'] leading-[57.60px]">$1,000</div>
+        </div>
+        <div className="w-[352px] h-[127px] left-[43px] top-[333px] absolute">
+        <div className="w-[350.29px] h-16 left-[1.71px] top-0 absolute text-black text-xl font-normal font-['Inter'] leading-normal">Deadline</div>
+        <div className="w-[340.04px] h-16 left-0 top-[63px] absolute text-black text-5xl font-bold font-['Inter'] leading-[57.60px]">28 Jan 2025</div>
+        </div>
+        <div className="w-[337px] h-[127px] left-[42px] top-[10px] absolute">
+        <div className="w-[336px] h-16 left-[1px] top-0 absolute text-black text-[32px] font-normal font-['Inter'] leading-[38.40px]">Funds Raised</div>
+        <div className="w-[336px] h-16 left-0 top-[63px] absolute text-black text-5xl font-bold font-['Inter'] leading-[57.60px]">$70,000</div>
+        </div>
+        <div className="w-[400px] h-20 left-[25px] top-[605px] absolute bg-black rounded-[20px] border border-white" />
+        <div className="w-[393px] h-[73px] left-[28px] top-[608px] absolute text-center text-white text-2xl font-semibold font-['Inter'] leading-[28.80px]">Invest in Rento</div>
+        <div className="w-[400px] h-20 left-[25px] top-[515px] absolute bg-white rounded-[20px] border border-black" />
+        <div className="w-[393px] h-[73px] left-[28px] top-[518px] absolute text-center text-black text-2xl font-semibold font-['Inter'] leading-[28.80px]">Request Data Room</div>
+        <div className="w-[380px] h-4 left-[35px] top-[156px] absolute bg-[#1a1a1a]/10 rounded-full">
+        <div className="w-[280.39px] h-4 left-[-5.34px] top-0 absolute bg-[#34c759]" />
+        </div>
+    </div>
     </div>
   );
 }
