15c4d54e5f8b6b31698a80edda8d4072ee471958
Phumrapee Chaowanapricha <phumrapeechaowanapricha@Phumrapees-MacBook-Pro-2.local>
2024-08-31 21:50:44 +0700

    implement tab bar for navigating between Pitch, Discussion, and Update sections

--- src/app/rento/page.tsx
+++ src/app/rento/page.tsx
@@ -7,55 +7,94 @@ const mockUrls = [
 
 export default function Page() {
   return (
-    <div className="flex flex-col font-[geist-sans] place-items-center my-10">
-        <div className="flex flex-row gap-2 place-items-center">
-            <div className="flex flex-col gap-10">
-                <div className="flex flex-row gap-2">
-                    <div className="relative md:w-[50px] md:h-[50px] sm:w-[25px] sm:h-[25px] my-5">
-                        <Image
-                            src="https://utfs.io/f/bb1dabab-7c7c-40d7-8ea5-030fdc7f1d96-ny8zu1.jpg"
-                            alt="Logo"
-                            layout="fill"
-                            objectFit="cover"
-                        />
-                    </div>
-                    <div className="flex flex-col my-3 mx-2">
-                        <div className="text-5xl">Rento</div>
-                        <div>Peer-to-Peer Rental Platform</div>
-                    </div>
-                </div>
-                <div className="relative md:w-[800px] md:h-[500px] sm:w-[400px] sm:h-[250px]">
-                    <Image
-                        src="https://utfs.io/f/7f073d8d-ade3-4ba3-ade5-165386c8a815-186s3o.png"
-                        alt="Logo"
-                        layout="fill"
-                        objectFit="cover"
-                    />
-                </div>
+    <div className="my-10 flex flex-col place-items-center font-[geist-sans]">
+      <div className="flex flex-row place-items-center gap-2">
+        <div className="flex flex-col gap-10">
+          <div className="flex flex-row gap-2">
+            <div className="relative my-5 sm:h-[25px] sm:w-[25px] md:h-[50px] md:w-[50px]">
+              <Image
+                src="https://utfs.io/f/bb1dabab-7c7c-40d7-8ea5-030fdc7f1d96-ny8zu1.jpg"
+                alt="Logo"
+                layout="fill"
+                objectFit="cover"
+              />
             </div>
-            <div className="flex flex-col ms-20">
-                <div>
-                    <div className="text-[32px]">Fund Raised</div>
-                    <div className="text-5xl">$332,160</div>
-                </div>
-                <div className="my-5">
-                    <div className="text-xl">investors</div>
-                    <div className="text-5xl">302</div>
-                </div>
-                <div className="my-3">
-                    <div className="text-xl">left to invest</div>
-                    <div className="text-5xl">4 hours</div>
-                </div>
-                <div className="my-5">
-                    <button className="bg-blue-700 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded">
-                        Invest in Rento
-                    </button>
-                </div>
+            <div className="mx-2 my-3 flex flex-col">
+              <div className="text-5xl">Rento</div>
+              <div>Peer-to-Peer Rental Platform</div>
             </div>
+          </div>
+          <div className="relative sm:h-[250px] sm:w-[400px] md:h-[500px] md:w-[800px]">
+            <Image
+              src="https://utfs.io/f/7f073d8d-ade3-4ba3-ade5-165386c8a815-186s3o.png"
+              alt="Logo"
+              layout="fill"
+              objectFit="cover"
+            />
+          </div>
         </div>
-        <div className="flex flex-col pt-5">
-            <div>PITCH HIGHLIGHT</div>
+        <div className="ms-20 flex flex-col">
+          <div>
+            <div className="text-[32px]">Fund Raised</div>
+            <div className="text-5xl">$332,160</div>
+          </div>
+          <div className="my-5">
+            <div className="text-xl">investors</div>
+            <div className="text-5xl">302</div>
+          </div>
+          <div className="my-3">
+            <div className="text-xl">left to invest</div>
+            <div className="text-5xl">4 hours</div>
+          </div>
+          <div className="my-5">
+            <button className="rounded bg-blue-700 px-4 py-2 font-bold text-white hover:bg-blue-500">
+              Invest in Rento
+            </button>
+          </div>
         </div>
+      </div>
+      <div className="border-b border-gray-200 my-10">
+        <nav
+          className="flex gap-x-1"
+          aria-label="Tabs"
+          role="tablist"
+          aria-orientation="horizontal"
+        >
+          <button
+            type="button"
+            className="hs-tab-active:font-semibold hs-tab-active:border-blue-600 hs-tab-active:text-blue-600 active inline-flex items-center gap-x-2 whitespace-nowrap border-b-2 border-transparent px-1 py-4 text-sm text-gray-500 hover:text-blue-600 focus:text-blue-600 focus:outline-none disabled:pointer-events-none disabled:opacity-50"
+            id="tabs-with-underline-item-1"
+            aria-selected="true"
+            data-hs-tab="#tabs-with-underline-1"
+            aria-controls="tabs-with-underline-1"
+            role="tab"
+          >
+            Pitch
+          </button>
+          <button
+            type="button"
+            className="hs-tab-active:font-semibold hs-tab-active:border-blue-600 hs-tab-active:text-blue-600 inline-flex items-center gap-x-2 whitespace-nowrap border-b-2 border-transparent px-1 py-4 text-sm text-gray-500 hover:text-blue-600 focus:text-blue-600 focus:outline-none disabled:pointer-events-none disabled:opacity-50"
+            id="tabs-with-underline-item-2"
+            aria-selected="false"
+            data-hs-tab="#tabs-with-underline-2"
+            aria-controls="tabs-with-underline-2"
+            role="tab"
+          >
+            Discussion
+          </button>
+          <button
+            type="button"
+            className="hs-tab-active:font-semibold hs-tab-active:border-blue-600 hs-tab-active:text-blue-600 inline-flex items-center gap-x-2 whitespace-nowrap border-b-2 border-transparent px-1 py-4 text-sm text-gray-500 hover:text-blue-600 focus:text-blue-600 focus:outline-none disabled:pointer-events-none disabled:opacity-50"
+            id="tabs-with-underline-item-3"
+            aria-selected="false"
+            data-hs-tab="#tabs-with-underline-3"
+            aria-controls="tabs-with-underline-3"
+            role="tab"
+          >
+            Update
+          </button>
+        </nav>
+      </div>
     </div>
   );
 }
