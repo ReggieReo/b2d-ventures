2f95447e3bc309aed462c1bd72f0f93bd085d2d2
Phumrapee Chaowanapricha <phumrapeechaowanapricha@Phumrapees-MacBook-Pro-2.local>
2024-09-02 00:23:38 +0700

    Adjust align text when smaller than mid screen

--- src/app/rento/page.tsx
+++ src/app/rento/page.tsx
@@ -42,7 +42,7 @@ export default function Page() {
             <div className="text-[24px] md:text-[32px]">Fund Raised</div>
             <div className="text-3xl md:text-5xl">$332,160</div>
             <div
-              className="flex h-1.5 w-full overflow-hidden rounded-full bg-gray-100 my-3"
+              className="my-3 flex h-1.5 w-full overflow-hidden rounded-full bg-gray-100"
               role="progressbar"
               aria-valuenow="25"
               aria-valuemin="0"
@@ -54,13 +54,15 @@ export default function Page() {
               ></div>
             </div>
           </div>
-          <div>
-            <div className="text-3xl md:text-5xl">302</div>
-            <div className="text-lg md:text-xl">investors</div>
-          </div>
-          <div>
-          <div className="text-3xl md:text-5xl">4 hours</div>
-            <div className="text-lg md:text-xl">left to invest</div>
+          <div className="flex flex-row justify-evenly gap-5 md:flex-col">
+            <div>
+              <div className="text-3xl md:text-5xl">302</div>
+              <div className="text-lg md:text-xl">investors</div>
+            </div>
+            <div>
+              <div className="text-3xl md:text-5xl">🔥 4 hours</div>
+              <div className="text-lg md:text-xl">left to invest</div>
+            </div>
           </div>
           <div>
             <button className="w-full rounded bg-blue-700 px-4 py-2 font-bold text-white hover:bg-blue-500">
@@ -86,28 +88,6 @@ export default function Page() {
             role="tab"
           >
             Pitch
-          </button>
-          <button
-            type="button"
-            className="hs-tab-active:font-bold hs-tab-active:border-blue-600 hs-tab-active:text-blue-600 inline-flex gap-x-2 whitespace-nowrap border-b-2 border-transparent px-4 py-4 text-base font-medium text-gray-600 hover:text-blue-600 focus:text-blue-600 focus:outline-none disabled:pointer-events-none disabled:opacity-50"
-            id="tabs-with-underline-item-2"
-            aria-selected="false"
-            data-hs-tab="#tabs-with-underline-2"
-            aria-controls="tabs-with-underline-2"
-            role="tab"
-          >
-            Discussion
-          </button>
-          <button
-            type="button"
-            className="hs-tab-active:font-bold hs-tab-active:border-blue-600 hs-tab-active:text-blue-600 inline-flex gap-x-2 whitespace-nowrap border-b-2 border-transparent px-4 py-4 text-base font-medium text-gray-600 hover:text-blue-600 focus:text-blue-600 focus:outline-none disabled:pointer-events-none disabled:opacity-50"
-            id="tabs-with-underline-item-3"
-            aria-selected="false"
-            data-hs-tab="#tabs-with-underline-3"
-            aria-controls="tabs-with-underline-3"
-            role="tab"
-          >
-            Update
           </button>
         </nav>
       </div>
