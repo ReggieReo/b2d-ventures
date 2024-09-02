e2193d76329b9674aaadb78c249556191ebd0e26
Phumrapee Chaowanapricha <phumrapeechaowanapricha@Phumrapees-MacBook-Pro-2.local>
2024-09-02 18:31:17 +0700

    rearrange portfolio page

--- src/app/rento/page.tsx
+++ src/app/rento/page.tsx
@@ -7,11 +7,11 @@ const mockUrls = [
 
 export default function Page() {
   return (
-    <div className="my-10 flex flex-col font-[geist-sans]">
+    <div className="my-10 flex flex-col font-geist-sans">
       <div className="flex flex-col place-content-center gap-10 md:flex-row">
-        <div className="flex w-full flex-col gap-10">
-          <div className="flex flex-row gap-2 p-3">
-            <div className="relative h-[100px] w-[100px] min-w-[100px]">
+        <div className="flex w-full flex-col gap-4 md:w-2/3">
+          <div className="mb-6 flex items-center gap-4">
+            <div className="relative h-[60px] w-[60px] min-w-[60px]">
               <Image
                 src="https://utfs.io/f/bb1dabab-7c7c-40d7-8ea5-030fdc7f1d96-ny8zu1.jpg"
                 alt="Logo"
@@ -19,17 +19,18 @@ export default function Page() {
                 objectFit="cover"
               />
             </div>
-            <div className="mx-2 my-3 flex flex-col md:items-start">
-              <div className="text-3xl md:text-5xl">Rento</div>
-              <div className="text-center md:text-left">
+            <div className="flex flex-col">
+              <div className="text-3xl font-bold md:text-4xl">Rento</div>
+              <div className="text-center text-sm md:text-left md:text-base">
                 Peer-to-Peer Rental Platform
               </div>
             </div>
           </div>
+
           <div>
             <Image
               src="https://utfs.io/f/7f073d8d-ade3-4ba3-ade5-165386c8a815-186s3o.png"
-              alt="Logo"
+              alt="Main Content"
               layout="responsive"
               objectFit="cover"
               width={1}
@@ -37,43 +38,49 @@ export default function Page() {
             />
           </div>
         </div>
-        <div className="mt-10 flex flex-col place-content-center gap-5 p-3 md:mt-0">
-          <div>
-            <div className="text-[24px] md:text-[32px]">Fund Raised</div>
-            <div className="text-3xl md:text-5xl">$332,160</div>
-            <div
-              className="my-3 flex h-1.5 w-full overflow-hidden rounded-full bg-gray-100"
-              role="progressbar"
-              aria-valuenow="25"
-              aria-valuemin="0"
-              aria-valuemax="100"
-            >
-              <div
-                className="flex flex-col justify-center overflow-hidden whitespace-nowrap rounded-full bg-blue-600 text-center text-xs text-white transition duration-500 dark:bg-green-500"
-                style={{ width: "25%" }}
-              ></div>
-            </div>
+
+        <div className="mt-16 md:mt-24 flex flex-col gap-5 p-3 md:w-1/5">
+          <div className="text-[24px] font-bold md:text-[28px]">
+            Fund Raised
           </div>
+          <div className="text-3xl font-bold md:text-4xl">$332,160</div>
+          <div className="text-sm text-gray-500 md:text-base">
+            6% raised of $5M max goal
+          </div>
+
+          <div
+            className="my-3 h-2 w-full overflow-hidden rounded-full bg-gray-200"
+            role="progressbar"
+            aria-valuenow="25"
+            aria-valuemin="0"
+            aria-valuemax="100"
+          >
+            <div className="h-full bg-green-600" style={{ width: "25%" }}></div>
+          </div>
+
           <div className="flex flex-row justify-evenly gap-5 md:flex-col">
             <div>
-              <div className="text-3xl md:text-5xl">302</div>
-              <div className="text-lg md:text-xl">investors</div>
+              <div className="text-3xl font-bold md:text-4xl">302</div>
+              <div className="text-lg md:text-xl">Investors</div>
             </div>
             <div>
-              <div className="text-3xl md:text-5xl">🔥 4 hours</div>
-              <div className="text-lg md:text-xl">left to invest</div>
+              <div className="text-3xl font-bold md:text-4xl">🔥 4 hours</div>
+              <div className="text-lg md:text-xl">Left to invest</div>
             </div>
           </div>
-          <div>
-            <button className="w-full rounded bg-blue-700 px-4 py-2 font-bold text-white hover:bg-blue-500">
-              Invest in Rento
-            </button>
+
+          <button className="w-full rounded bg-blue-700 px-4 py-2 font-bold text-white hover:bg-blue-500">
+            Invest in Rento
+          </button>
+          <div className="mt-2 text-center text-sm text-gray-500">
+            $300 minimum investment
           </div>
         </div>
       </div>
+
       <div className="my-8 border-b border-gray-200">
         <nav
-          className="flex justify-start gap-x-5"
+          className="flex justify-start gap-x-5 w-full max-w-[90%] mx-auto"
           aria-label="Tabs"
           role="tablist"
           aria-orientation="horizontal"
