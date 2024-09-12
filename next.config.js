53843a0ed8bbfc0a3d2935ae3ca01dff9ad580cf
Phumrapee Chaowanapricha <phumrapeechaowanapricha@Phumrapees-MacBook-Pro-2.local>
2024-09-12 14:03:51 +0700

    update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,6 +5,16 @@
 await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'utfs.io',
+                pathname: '/f/**',
+            }
+        ]
+    }
+};
 
 export default config;
