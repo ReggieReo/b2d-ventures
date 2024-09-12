339f8376e61e2ff802195855092ccc205df14b7a
Phumrapee Chaowanapricha <phumrapeechaowanapricha@Phumrapees-MacBook-Pro-2.local>
2024-09-12 14:14:31 +0700

    Modify text

--- src/app/investform/page.tsx
+++ src/app/investform/page.tsx
@@ -5,11 +5,24 @@ const mockUrls = [
 ];
 
 export default function Page() {
-    return (
-        <div className="my-20 gap-4 flex flex-col place-items-center">
-            <div>Logo & Invest in Rento</div>
-            <div>Payment information</div>
-            <div>Terms</div>
+  return (
+    <div className="my-20 flex flex-col place-items-center gap-4">
+      <div className="mb-6 flex items-center gap-4">
+        <div className="relative h-[40px] w-[40px] min-w-[40px]">
+          <Image
+            src="https://utfs.io/f/bb1dabab-7c7c-40d7-8ea5-030fdc7f1d96-ny8zu1.jpg"
+            alt="Logo"
+            layout="fill"
+            objectFit="cover"
+          />
         </div>
-    );
+        <div className="flex flex-col">
+          <div className="text-3xl font-bold md:text-4xl">Invest in Rento</div>
+        </div>
+      </div>
+      <div className="text-3xl font-bold md:text-2xl">Investment amount</div>
+      <div className="text-3xl font-bold md:text-2xl">Payment information</div>
+      <div className="text-3xl font-bold md:text-2xl">Terms</div>
+    </div>
+  );
 }
