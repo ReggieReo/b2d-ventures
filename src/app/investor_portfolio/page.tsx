a0c8284644ceb3733cc61ef80b1e19adf0c78dca
Nanthawat Duang-ead <33195267+D7NAMITE@users.noreply.github.com>
2024-09-14 21:29:36 +0700

    Edit the Portfolio Summary Layout

--- src/app/investor_portfolio/page.tsx
+++ src/app/investor_portfolio/page.tsx
@@ -13,59 +13,58 @@ export default async function InvestorPortfolioPage() {
   return (
     <main className="justify-left items-left mb-4 ml-4 mt-4 flex min-h-screen flex-col">
       <p className={"mb-5 text-3xl font-bold"}>My Business Portfolio</p>
-      <div className={"flex flex-row gap-4"}>
-        <Card>
-          <CardHeader>
-            <CardTitle>Portfolio Summary</CardTitle>
-            <CardDescription>
-              A brief summary of the investment portfolio
-            </CardDescription>
-          </CardHeader>
-          <CardContent className={"flex flex-row"}>
-            <div className={"mb-4 ml-4 flex flex-col gap-y-3"}>
-              <div>
-                <p>Total:</p>
-                <p className={"text-xl font-bold"}>$2,250</p>
+        <div className={"flex flex-row gap-4 justify-center"}>
+          <Card>
+            <CardHeader>
+              <CardTitle>Portfolio Summary</CardTitle>
+              <CardDescription>
+                A brief summary of the investment portfolio
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <div className={"flex flex-col items-center"}>
+                <p className={"text-2xl"}>Total:</p>
+                <p className={"text-3xl font-bold"}>$2,250</p>
               </div>
-              <div>
-                <p>Media:</p>
-                <p className={"text-xl font-bold"}>$150</p>
+              <div className={"mt-4 flex flex-row justify-center gap-x-5"}>
+                <div className={"flex flex-col gap-2"}>
+                  <div className={"flex flex-col"}>
+                    <p className={"mr-1"}>A1-sector:</p>
+                    <p className={"font-bold"}>$1,000</p>
+                  </div>
+                  <div className={"flex flex-col"}>
+                    <p className={"mr-1"}>A2-sector:</p>
+                    <p className={"font-bold"}>$1,000</p>
+                  </div>
+                  <div className={"flex flex-col"}>
+                    <p className={"mr-1"}>A3-sector:</p>
+                    <p className={"font-bold"}>$1,000</p>
+                  </div>
+                </div>
+                <div className={"flex flex-col gap-2"}>
+                  <div className={"flex flex-col"}>
+                    <p className={"mr-1"}>B1-sector:</p>
+                    <p className={"font-bold"}>$1,000</p>
+                  </div>
+                  <div className={"flex flex-col"}>
+                    <p className={"mr-1"}>B2-sector:</p>
+                    <p className={"font-bold"}>$1,000</p>
+                  </div>
+                  <div className={"flex flex-col"}>
+                    <p className={"mr-1"}>B3-sector:</p>
+                    <p className={"font-bold"}>$1,000</p>
+                  </div>
+                </div>
+
               </div>
-              <div>
-                <p>Technology</p>
-                <p className={"text-xl font-bold"}>$450</p>
-              </div>
-              <div>
-                <p>Gaming</p>
-                <p className={"text-xl font-bold"}>$200</p>
-              </div>
-            </div>
-            <div className={"mb-4 flex flex-col gap-y-3"}>
-              <div>
-                <p>Finance and Fintech:</p>
-                <p className={"text-xl font-bold"}>$250</p>
-              </div>
-              <div>
-                <p>Healthcare:</p>
-                <p className={"text-xl font-bold"}>$350</p>
-              </div>
-              <div>
-                <p>Energy</p>
-                <p className={"text-xl font-bold"}>$550</p>
-              </div>
-              <div>
-                <p>Real Estate</p>
-                <p className={"text-xl font-bold"}>$300</p>
-              </div>
-            </div>
-          </CardContent>
-        </Card>
-        <Card>
-          <CardHeader>
-            <CardTitle>Investment Breakdown</CardTitle>
-          </CardHeader>
-        </Card>
-      </div>
+            </CardContent>
+          </Card>
+          <Card>
+            <CardHeader>
+              <CardTitle>Investment Breakdown</CardTitle>
+            </CardHeader>
+          </Card>
+        </div>
     </main>
   );
 }
