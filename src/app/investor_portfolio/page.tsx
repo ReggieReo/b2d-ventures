b05c829035d46fad878792675a511b5824729574
Nanthawat Duang-ead <nanthawat.boom@gmail.com>
2024-09-18 11:02:28 +0700

    Add Investment Visualization Section and Investment Breakdown Section

--- src/app/investor_portfolio/page.tsx
+++ src/app/investor_portfolio/page.tsx
@@ -7,64 +7,74 @@ import {
   CardTitle,
 } from "~/components/ui/card";
 
+import { InvestorPortPieChart } from "~/app/_component/pie_chart";
+import { InvestmentHistoryTable } from "~/app/_component/investment_history";
+
 export const dynamic = "force-dynamic";
 
 export default async function InvestorPortfolioPage() {
   return (
     <main className="justify-left items-left mb-4 ml-4 mt-4 flex min-h-screen flex-col">
       <p className={"mb-5 text-3xl font-bold"}>My Business Portfolio</p>
-        <div className={"flex flex-row gap-4 justify-center"}>
-          <Card>
-            <CardHeader>
-              <CardTitle>Portfolio Summary</CardTitle>
-              <CardDescription>
-                A brief summary of the investment portfolio
-              </CardDescription>
-            </CardHeader>
-            <CardContent>
-              <div className={"flex flex-col items-center"}>
-                <p className={"text-2xl"}>Total:</p>
-                <p className={"text-3xl font-bold"}>$2,250</p>
-              </div>
-              <div className={"mt-4 flex flex-row justify-center gap-x-5"}>
-                <div className={"flex flex-col gap-2"}>
-                  <div className={"flex flex-col"}>
-                    <p className={"mr-1"}>A1-sector:</p>
-                    <p className={"font-bold"}>$1,000</p>
-                  </div>
-                  <div className={"flex flex-col"}>
-                    <p className={"mr-1"}>A2-sector:</p>
-                    <p className={"font-bold"}>$1,000</p>
-                  </div>
-                  <div className={"flex flex-col"}>
-                    <p className={"mr-1"}>A3-sector:</p>
-                    <p className={"font-bold"}>$1,000</p>
-                  </div>
+      <div
+        className={
+          "flex w-screen max-w-7xl flex-row justify-center gap-4 self-center"
+        }
+      >
+        <Card className={"w-1/2"}>
+          <CardHeader>
+            <CardTitle>Portfolio Summary</CardTitle>
+            <CardDescription>
+              A brief summary of the investment portfolio
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <div className={"flex flex-col items-center"}>
+              <p className={"text-2xl"}>Total:</p>
+              <p className={"text-3xl font-bold"}>$2,250</p>
+            </div>
+            <div className={"mt-4 flex flex-row justify-center gap-x-5"}>
+              <div className={"flex flex-col gap-2"}>
+                <div className={"flex flex-col"}>
+                  <p className={"mr-1"}>A1-sector:</p>
+                  <p className={"font-bold"}>$1,000</p>
                 </div>
-                <div className={"flex flex-col gap-2"}>
-                  <div className={"flex flex-col"}>
-                    <p className={"mr-1"}>B1-sector:</p>
-                    <p className={"font-bold"}>$1,000</p>
-                  </div>
-                  <div className={"flex flex-col"}>
-                    <p className={"mr-1"}>B2-sector:</p>
-                    <p className={"font-bold"}>$1,000</p>
-                  </div>
-                  <div className={"flex flex-col"}>
-                    <p className={"mr-1"}>B3-sector:</p>
-                    <p className={"font-bold"}>$1,000</p>
-                  </div>
+                <div className={"flex flex-col"}>
+                  <p className={"mr-1"}>A2-sector:</p>
+                  <p className={"font-bold"}>$1,000</p>
                 </div>
-
+                <div className={"flex flex-col"}>
+                  <p className={"mr-1"}>A3-sector:</p>
+                  <p className={"font-bold"}>$1,000</p>
+                </div>
               </div>
-            </CardContent>
-          </Card>
-          <Card>
-            <CardHeader>
-              <CardTitle>Investment Breakdown</CardTitle>
-            </CardHeader>
-          </Card>
-        </div>
+              <div className={"flex flex-col gap-2"}>
+                <div className={"flex flex-col"}>
+                  <p className={"mr-1"}>B1-sector:</p>
+                  <p className={"font-bold"}>$1,000</p>
+                </div>
+                <div className={"flex flex-col"}>
+                  <p className={"mr-1"}>B2-sector:</p>
+                  <p className={"font-bold"}>$1,000</p>
+                </div>
+                <div className={"flex flex-col"}>
+                  <p className={"mr-1"}>B3-sector:</p>
+                  <p className={"font-bold"}>$1,000</p>
+                </div>
+              </div>
+            </div>
+          </CardContent>
+        </Card>
+        <InvestorPortPieChart />
+      </div>
+      <div
+        className={
+          "mt-4 flex w-screen max-w-7xl flex-col justify-center gap-4 self-center"
+        }
+      >
+        <p className={"text-2xl font-bold"}>Investment Breakdown</p>
+        <InvestmentHistoryTable />
+      </div>
     </main>
   );
 }
