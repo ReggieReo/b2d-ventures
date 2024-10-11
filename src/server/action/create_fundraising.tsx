cd1a3f088629cb398a9b9893f2cb81f661c2d592
Setthapon Thadisakun <103312026+ReggieReo@users.noreply.github.com>
2024-10-11 15:02:10 +0700

    26 implement sending creating fundraising form to backend (#27)
    * init sending data to backend
    
    * add form validation on server
    
    * update schema for buisiness
    
    * refactor file directory and insert data into db
    
    * delete default values

---
+++ src/server/action/create_fundraising.tsx
@@ -0,0 +1,31 @@
+"use server";
+
+import { auth } from "@clerk/nextjs/server";
+import { formSchema } from "~/app/create_fundraising/schema";
+import { z } from "zod";
+
+import { createBusiness } from "~/server/createQuery";
+
+export async function createFundraising(formData: FormData) {
+  "use server";
+
+  const validatedFields = formSchema.safeParse({
+    company: formData.get("company"),
+    title: formData.get("title"),
+    website: formData.get("website"),
+    target_fund: formData.get("target_fund:"),
+    min_investment: formData.get("min_investment"),
+    allocation: formData.get("allocation"),
+    valuation: formData.get("valuation"),
+    deadline: formData.get("deadline"),
+    email: formData.get("email"),
+    industry: formData.get("industry"),
+  });
+
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+    };
+  }
+  await createBusiness(validatedFields.data);
+}
