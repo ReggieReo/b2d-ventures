3b4ab8ffc883c7068465da7f7c2e2e1a88750e33
Nanthawat Duang-ead <33195267+D7NAMITE@users.noreply.github.com>
2024-10-14 15:36:22 +0700

    add progress bar

--- src/app/startup_dashboard/page.tsx
+++ src/app/startup_dashboard/page.tsx
@@ -25,33 +25,40 @@ export default async function StartupDashboard() {
             <CardDescription>Last Updated: Date</CardDescription>
           </CardHeader>
           <CardContent>
-            <div className={"flex flex-row justify-center gap-x-10"}>
-              <div className={"flex-col"}>
-                <p className={"text-2xl"}>Fund Raised</p>
-                <div className={"ml-3 flex flex-row items-end gap-2"}>
-                  {/*TODO: Implement logic for total fund raised*/}
-                  <p className={"text-3xl font-bold"}>$10,000</p>
-                  {/*TODO: Implement logic for fund diff to last week*/}
-                  <p>(+$1000 from last week)</p>
+            <div className={"flex flex-col gap-y-5"}>
+              <div className={"flex flex-row gap-x-10"}>
+                <div className={"flex-col"}>
+                  <p className={"text-2xl"}>Fund Raised</p>
+                  <div className={"ml-3 flex flex-row items-end gap-2"}>
+                    {/*TODO: Implement logic for total fund raised*/}
+                    <p className={"text-3xl font-bold"}>$10,000</p>
+                    {/*TODO: Implement logic for fund diff to last week*/}
+                    <p>(+$1000 from last week)</p>
+                  </div>
+                </div>
+                <div className={"flex flex-col"}>
+                  <p className={"text-2xl"}>Investor</p>
+                  <div className={"ml-3 flex flex-row items-end gap-2"}>
+                    {/*TODO: Implement logic for Total investor*/}
+                    <p className={"text-3xl font-bold"}>41</p>
+                    {/*TODO: Implement logic for diff investor to last week*/}
+                    <p>(+3 from last week)</p>
+                  </div>
+                </div>
+                <div className={"flex flex-col"}>
+                  <p className={"text-2xl"}>Day to go</p>
+                  <div className={"ml-3 flex flex-row items-end gap-2"}>
+                    {/*TODO: Implement logic for Day left*/}
+                    <p className={"text-3xl font-bold"}>75</p>
+                    {/*TODO: Implement logic for day from start*/}
+                    <p>(15 days from start)</p>
+                  </div>
                 </div>
               </div>
-              <div className={"flex-col"}>
-                <p className={"text-2xl"}>Investor</p>
-                <div className={"ml-3 flex flex-row items-end gap-2"}>
-                  {/*TODO: Implement logic for Total investor*/}
-                  <p className={"text-3xl font-bold"}>41</p>
-                  {/*TODO: Implement logic for diff investor to last week*/}
-                  <p>(+3 from last week)</p>
-                </div>
-              </div>
-              <div className={"flex-col"}>
-                <p className={"text-2xl"}>Day to go</p>
-                <div className={"ml-3 flex flex-row items-end gap-2"}>
-                  {/*TODO: Implement logic for Day left*/}
-                  <p className={"text-3xl font-bold"}>75</p>
-                  {/*TODO: Implement logic for day from start*/}
-                  <p>(15 days from start)</p>
-                </div>
+              <div className={"ml-5 flex flex-row gap-x-5 items-center"}>
+                {/*TODO: Implement fundraised percentage logic*/}
+                <p>32%</p>
+                <Progress value={32} className="w-[60%]"/>
               </div>
             </div>
           </CardContent>
@@ -65,8 +72,12 @@ export default async function StartupDashboard() {
           </CardHeader>
           <CardContent>
             <div className={"flex flex-row"}>
-              <div className={"flex-col"}>
-                <div className={"flex-col"}>
+              <div className={"flex flex-row gap-x-10"}>
+                <div className={"flex flex-col"}>
+                  <p className={"text-xl"}>Minimum Check-size</p>
+                  <p className={"text-2xl font-bold"}>$100</p>
+                </div>
+                <div className={"flex flex-col"}>
                   <p className={"text-xl"}>Minimum Check-size</p>
                   <p className={"text-2xl font-bold"}>$100</p>
                 </div>
