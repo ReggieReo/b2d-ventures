90e7675986dacd4cb66d30e95c39637205732e12
Setthapon Thadisakun <103312026+ReggieReo@users.noreply.github.com>
2024-10-14 15:13:59 +0700

    28 implement linking fundraising listing logic into browsing page (#29)
    * add card component for business
    
    * update package
    
    * update package again
    
    * reset package
    
    * update uploadthing
    
    * test building
    
    * run on every push
    
    * create env for action
    
    * only run action on pull request
    
    * update ignore lint file

--- src/server/fetchQuery.ts
+++ src/server/fetchQuery.ts
@@ -1,5 +1,5 @@
-import { db } from "~/server/db";
 import "server-only";
+import { db } from "~/server/db";
 
 // user client -> ship js to the client but code still on the server
 // user server -> expose endpoint to the client
@@ -8,6 +8,17 @@ import "server-only";
 export async function getUser() {
   return db.query.user.findMany();
 }
+
 export async function getAllImages() {
   return db.query.media.findMany();
+}
+
+export async function getAllBusiness() {
+  return db.query.business.findMany();
+}
+
+export async function getInvestmentByBusinessID(businessID: number) {
+  return db.query.investment.findMany({
+    where: (model, { eq }) => eq(model.businessID, businessID),
+  });
 }
