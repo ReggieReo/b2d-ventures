383ad12904371508a0cf92353dfb6570f96c099a
Phumrapee Chaowanapricha <phumrapee.ch4352@gmail.com>
2024-11-09 22:35:55 +0700

    Modify component

--- src/app/admin/fund/page.tsx
+++ src/app/admin/fund/page.tsx
@@ -1,9 +1,10 @@
-import { getAllBusiness } from "~/server/fetchQuery"
+import { getPendingBusinesses } from "~/server/fetchQuery"
 import BusinessTable from "~/components/business_table"
-import {CampaignData} from "~/models/fund";
-export default async function Fund(){
-  const data = await getAllBusiness() as unknown as CampaignData[];
+import { Business } from "~/components/business_table"
 
+export default async function Fund(){
+  const data = await getPendingBusinesses() as unknown as Business[];
+  console.log(data)
   return (
     <BusinessTable data = {data} />
   )
