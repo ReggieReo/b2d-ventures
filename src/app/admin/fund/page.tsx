49d020e4a1d5246adcb953eecb2b91eacedc4626
Phumrapee Chaowanapricha <phumrapee.ch4352@gmail.com>
2024-11-10 12:25:55 +0700

    Modify fund page

--- src/app/admin/fund/page.tsx
+++ src/app/admin/fund/page.tsx
@@ -4,7 +4,6 @@ import { Business } from "~/components/business_table"
 
 export default async function Fund(){
   const data = await getPendingBusinesses() as unknown as Business[];
-  console.log(data)
   return (
     <BusinessTable data = {data} />
   )
