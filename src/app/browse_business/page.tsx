2e114b4dc846081b3b9620f417ec13760c271b9f
Nanthawat Duang-ead <33195267+D7NAMITE@users.noreply.github.com>
2024-11-15 20:48:14 +0700

    add sorting select

--- src/app/browse_business/page.tsx
+++ src/app/browse_business/page.tsx
@@ -1,6 +1,6 @@
 "use server";
 import {
-  getAcceptBusinessesByName,
+  getAcceptBusinessesByKeyData,
   getAcceptedBusinesses,
   getBusinessByIndustries,
 } from "~/server/fetchQuery";
@@ -9,6 +9,15 @@ import BusinessCard from "~/components/business_card";
 import SearchBusinessInput from "~/components/search_business_input";
 import SearchBusinessFilter from "~/components/search_filter";
 import { BrowsePagePagination } from "~/components/browsePagePagination";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "~/components/ui/select";
 
 export default async function HomePage(props: {
   searchParams?: Promise<{
@@ -25,9 +34,12 @@ export default async function HomePage(props: {
 
   const industries = industryParam ? industryParam.split(",") : [];
 
-
-  const business = await getAcceptBusinessesByName(query, currentPage, businessPerPage, industries);
-
+  const business = await getAcceptBusinessesByKeyData(
+    query,
+    currentPage,
+    businessPerPage,
+    industries,
+  );
 
   const listBusiness = await getAcceptedBusinesses();
   let totalBusiness = listBusiness.length;
@@ -37,7 +49,6 @@ export default async function HomePage(props: {
 
   const totalPages = Math.ceil(totalBusiness / businessPerPage);
 
-
   return (
     <main className={"mt-12 flex h-screen w-screen flex-col items-center"}>
       <div className={"mb-4 flex w-full max-w-5xl flex-col"}>
@@ -46,7 +57,33 @@ export default async function HomePage(props: {
       </div>
       <div className={"mb-8 flex w-full max-w-5xl flex-col items-start gap-2"}>
         <SearchBusinessInput />
-        <SearchBusinessFilter />
+        <div className={"flex flex-row gap-x-5"}>
+          <SearchBusinessFilter />
+          <div className={"flex flex-row gap-x-2"}>
+          <Select>
+            <SelectTrigger className="w-[280px]">
+              <SelectValue placeholder="Sort By" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="most_recent">Created</SelectItem>
+              <SelectItem value="deadline">Deadline</SelectItem>
+              <SelectItem value="no_investor">Number of Investor</SelectItem>
+              <SelectItem value="anount_invest">Amount Invested</SelectItem>
+              <SelectItem value="remaining stocks">Remaining Stocks</SelectItem>
+              <SelectItem value="min_invest">Minimum Investment</SelectItem>
+            </SelectContent>
+          </Select>
+          <Select>
+            <SelectTrigger className="w-[280px]">
+              <SelectValue placeholder="Order" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="asc">Ascending</SelectItem>
+              <SelectItem value="desc">Descending</SelectItem>
+            </SelectContent>
+          </Select>
+          </div>
+        </div>
       </div>
       <div className="grid max-w-5xl grid-cols-1 items-stretch gap-10 md:grid-cols-2 lg:grid-cols-3">
         {business.map((b) => (
