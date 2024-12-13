447ac4c96c43df401782784ed7531ad3ebade051
Setthapon Thadisakun <steam.reaw1@gmail.com>
2024-12-13 13:51:45 +0700

    add secure header

--- next.config.js
+++ next.config.js
@@ -19,6 +19,19 @@ const config = {
   },
   pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
   transpilePackages: ["next-mdx-remote"],
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          { key: "X-Frame-Options", value: "DENY" },
+          { key: "X-Content-Type-Options", value: "nosniff" },
+          { key: "Strict-Transport-Security", value: "max-age=63072000; includeSubDomains" },
+          { key: "Cache-Control", value: "no-cache, no-store, max-age=0, must-revalidate" },
+        ],
+      },
+    ];
+  },
 };
 
 const withMDX = createMDX({
